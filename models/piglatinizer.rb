7d8f37a28320f9d7fa4ef9d838da12dcef63464a
Naz Khan <nazmuskhan@yahoo.com>
2017-04-05 00:27:02 -0400

    Done.

---
+++ models/piglatinizer.rb
@@ -0,0 +1,70 @@
+require 'pry'
+
+class PigLatinizer
+
+  attr_accessor :text
+
+  @@vowels = ['a','e','i','o','u','A','E','I','O','U']
+
+
+  def piglatinize(word)
+    categorize_then_operate(word)
+  end
+
+  def to_pig_latin(word)
+    check_for_sentences(word).join(' ')
+  end
+
+  private
+
+  def check_for_sentences(word)
+    if word.split(' ').count > 1
+      word.split(' ').map do |word|
+        categorize_then_operate(word)
+      end
+    end
+  end
+
+  def vowelplize(word)
+    word + "way"
+  end
+
+  def clusterplize(word)
+    cutoff_index = 0
+    letters_chopped = ""
+    word.split('').each_with_index do |letter, index|
+      if @@vowels.include?(letter)
+        cutoff_index = index
+        break
+      end
+    end
+      while cutoff_index > 0
+        forgotten = word[0]
+        word.slice!(word[0])
+        word << forgotten
+        cutoff_index -= 1
+      end
+      word << "ay"
+    end
+
+  def regplize(word)
+    first_letter = word[0]
+    word.slice!(first_letter)
+    word << first_letter
+    word << "ay"
+  end
+
+  def categorize_then_operate(word)
+    if @@vowels.include?(word[0])
+      vowelplize(word)
+    elsif @@vowels.include?(word[1])
+      regplize(word)
+    else
+      clusterplize(word)
+    end
+  end
+
+end
+
+string = PigLatinizer.new
+puts string.piglatinize("baby")
